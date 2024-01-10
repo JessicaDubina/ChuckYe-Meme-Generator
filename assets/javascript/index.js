bd0d5236e1b709db9a579e9b03f8d319b074b248
unknown <rhett.gibbs@yahoo.com>
2024-01-10 14:46:47 -0800

    Added a bunch of functions to Javascript

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -4,7 +4,8 @@ let chuckQuoteEl = document.querySelector("#chuck-quote");
 let gifHolderEl = document.querySelector("#gif-holder");
 let selectedQuoteEl = document.querySelector("#selected-quote");
 
-let index = 3;
+let index = 4;
+let searchLimit = 50;
 let holdData;
 
 let dataObjectNames = {
@@ -15,21 +16,21 @@ let dataObjectNames = {
     4: "art & design",
     5: "cartoons & comics",
     6: "celebrities",
-    7: "decades",
+    7: "decades",//?
     8: "emotions",
     9: "fashion & beauty",
     10: "food & drink",
     11: "gaming",
     12: "greetings",
     13: "holiday",
-    14: "identity",
+    14: "identity",//?
     15: "interests",
     16: "memes",
     17: "movies",
     18: "music",
     19: "nature",
     20: "news & politics",
-    21: "reactions",
+    21: "reactions",//?
     22: "science",
     23: "sports",
     24: "stickers",
@@ -38,6 +39,11 @@ let dataObjectNames = {
     27: "weird",
 };
 
+var randomEndpointKey = "https://api.giphy.com/v1/gifs/random"
+var searchCategoriesKey = "https://api.giphy.com/v1/gifs/categories?api_key=bKFrNvQBG7WJdUKyt4cnTcta9Q84q8ks";
+var searchEndpointKey = "https://api.giphy.com/v1/gifs/search?limit=" + searchLimit + "&q=" 
++ dataObjectNames[index] + "&api_key=bKFrNvQBG7WJdUKyt4cnTcta9Q84q8ks";
+
 var chuckNorrisKey = "https://api.kanye.rest";
 fetch(chuckNorrisKey, {
     method: 'GET',
@@ -45,7 +51,6 @@ fetch(chuckNorrisKey, {
     return response.json();
 }).then(function(data) {
     //Kanye quote here
-    console.log(data);
     kanyeQuoteEl.textContent = "Kanye Says: " + data.quote;
 });
 
@@ -55,7 +60,6 @@ fetch("https://api.chucknorris.io/jokes/random", {
     return response.json();
 }).then(function(data) {
     //Chuck quote here
-    console.log(data);
     chuckQuoteEl.textContent = "Chuck Says: " + data.value;
 });
 
@@ -70,9 +74,51 @@ fetch("https://api.chucknorris.io/jokes/random", {
 */
 //giphy api key = bKFrNvQBG7WJdUKyt4cnTcta9Q84q8ks
 
-var searchCategoriesKey = "https://api.giphy.com/v1/gifs/categories?api_key=bKFrNvQBG7WJdUKyt4cnTcta9Q84q8ks";
-var searchEndpointKey = "https://api.giphy.com/v1/gifs/search?api_key=bKFrNvQBG7WJdUKyt4cnTcta9Q84q8ks?limit=20?q=" 
-+ dataObjectNames[index];
+const SetIndex = (input) => {
+    index = input;
+}
+
+const FetchSearchData = () => {
+    //index = Math.floor(Math.random() * Object.keys(dataObjectNames).length);
+    index = 27;
+    searchEndpointKey = "https://api.giphy.com/v1/gifs/search?limit=" + searchLimit + "&q=" 
+    + dataObjectNames[index] + "&api_key=bKFrNvQBG7WJdUKyt4cnTcta9Q84q8ks";
+
+    fetch(searchEndpointKey, {
+        method: 'GET',
+    }).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        //var random = data.data.length -1;
+        console.log(data);
+        holdData = data;
+        AppendGifToPageAlt();
+    });
+
+    let CycleGif = setInterval(function() {
+        index++;
+        if(index > dataObjectNames.length) { index = 0; }
+        AppendGifToPageAlt();
+    }, 10000);
+}
+
+const FetchCategoryData = () => {
+    fetch(searchCategoriesKey, {
+        method: 'GET',
+    }).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        console.log(data);
+        holdData = data;
+        AppendGifToPage();
+    });
+
+    let CycleGif = setInterval(function() {
+        index++;
+        if(index > dataObjectNames.length) { index = 0; }
+        AppendGifToPage();
+    }, 10000);
+}
 
 const AppendGifToPage = () => {
     var check = document.querySelector(".gif-parent");
@@ -85,20 +131,20 @@ const AppendGifToPage = () => {
     gifHolderEl.append(gifParent);
 }
 
-fetch(searchCategoriesKey, {
-    method: 'GET',
-}).then(function(response) {
-    return response.json();
-}).then(function(data) {
-    console.log(data);
-    holdData = data;
-    AppendGifToPage();
-});
+const AppendGifToPageAlt = () => {
+    var check = document.querySelector(".gif-parent");
+    if(check) { check.remove(); }
 
-let CycleGif = setInterval(function() {
-    index++;
-    if(index > dataObjectNames.length) {
-        index = 0;
-    }
-    AppendGifToPage();
-}, 10000);
+    var gifParent = document.createElement("div");
+    var gifHolder = document.createElement("img");
+    var random = Math.floor(Math.random() * holdData.data.length);
+    console.log(index + " : " + random);
+
+    gifHolder.src = holdData.data[random].images.downsized_large.url;
+
+    gifParent.classList.add("gif-parent");
+    gifParent.append(gifHolder);
+    gifHolderEl.append(gifParent);
+}
+
+FetchSearchData();

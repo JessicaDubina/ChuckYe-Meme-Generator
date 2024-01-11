7e50747a773fee39042514d2ae6abc81c88fa525
unknown <rhett.gibbs@yahoo.com>
2024-01-11 11:38:49 -0800

    Most of the search functionality completed - added a few more functions to handle and set data

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -4,10 +4,6 @@ let chuckQuoteEl = document.querySelector("#chuck-quote");
 let gifHolderEl = document.querySelector("#gif-holder");
 let selectedQuoteEl = document.querySelector("#selected-quote");
 
-let index = 4;
-let searchLimit = 50;
-let holdData;
-
 let dataObjectNames = {
     0: "actions",
     1: "adjectives",
@@ -39,29 +35,7 @@ let dataObjectNames = {
     27: "weird",
 };
 
-var randomEndpointKey = "https://api.giphy.com/v1/gifs/random"
-var searchCategoriesKey = "https://api.giphy.com/v1/gifs/categories?api_key=bKFrNvQBG7WJdUKyt4cnTcta9Q84q8ks";
-var searchEndpointKey = "https://api.giphy.com/v1/gifs/search?limit=" + searchLimit + "&q=" 
-+ dataObjectNames[index] + "&api_key=bKFrNvQBG7WJdUKyt4cnTcta9Q84q8ks";
 
-var chuckNorrisKey = "https://api.kanye.rest";
-fetch(chuckNorrisKey, {
-    method: 'GET',
-}).then(function(response) {
-    return response.json();
-}).then(function(data) {
-    //Kanye quote here
-    kanyeQuoteEl.textContent = "Kanye Says: " + data.quote;
-});
-
-fetch("https://api.chucknorris.io/jokes/random", {
-    method: 'GET',
-}).then(function(response) {
-    return response.json();
-}).then(function(data) {
-    //Chuck quote here
-    chuckQuoteEl.textContent = "Chuck Says: " + data.value;
-});
 
 /*fetch("https://stoic.tekloon.net/stoic-quote", {
     method: 'GET',
@@ -76,29 +50,66 @@ fetch("https://api.chucknorris.io/jokes/random", {
 
 const SetIndex = (input) => {
     index = input;
+    console.log("Index Set: " + index);
+}
+
+const SetSearchParam = (input) => {
+    searchQuestion = input;
+}
+
+let index = 4;
+let searchLimit = 50;
+let holdData;
+let searchQuestion = dataObjectNames[index];
+
+const kanyeKey = "https://api.kanye.rest";
+const chuckKey = "https://api.chucknorris.io/jokes/random";
+var randomEndpointKey = "https://api.giphy.com/v1/gifs/random"
+var searchCategoriesKey = "https://api.giphy.com/v1/gifs/categories?api_key=bKFrNvQBG7WJdUKyt4cnTcta9Q84q8ks";
+var searchEndpointKey = "https://api.giphy.com/v1/gifs/search?limit=" + searchLimit + "&q=" 
++ searchQuestion + "&api_key=bKFrNvQBG7WJdUKyt4cnTcta9Q84q8ks";
+
+const FetchQuotes = () => {
+    fetch(kanyeKey, {
+        method: 'GET',
+    }).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        //Kanye quote here
+        kanyeQuoteEl.textContent = "Kanye Says: " + data.quote;
+    });
+    
+    fetch(chuckKey, {
+        method: 'GET',
+    }).then(function(response) {
+        return response.json();
+    }).then(function(data) {
+        //Chuck quote here
+        chuckQuoteEl.textContent = "Chuck Says: " + data.value;
+    });
 }
 
 const FetchSearchData = () => {
-    index = Math.floor(Math.random() * Object.keys(dataObjectNames).length);
+    //randomize category?
+    //index = Math.floor(Math.random() * Object.keys(dataObjectNames).length);
     searchEndpointKey = "https://api.giphy.com/v1/gifs/search?limit=" + searchLimit + "&q=" 
-    + dataObjectNames[index] + "&api_key=bKFrNvQBG7WJdUKyt4cnTcta9Q84q8ks";
+    + searchQuestion + "&api_key=bKFrNvQBG7WJdUKyt4cnTcta9Q84q8ks";
 
     fetch(searchEndpointKey, {
         method: 'GET',
     }).then(function(response) {
         return response.json();
     }).then(function(data) {
-        //var random = data.data.length -1;
         console.log(data);
         holdData = data;
         AppendGifToPageAlt();
     });
 
-    let CycleGif = setInterval(function() {
+    /*let CycleGif = setInterval(function() {
         index++;
         if(index > dataObjectNames.length) { index = 0; }
         AppendGifToPageAlt();
-    }, 10000);
+    }, 10000);*/
 }
 
 const FetchCategoryData = () => {
@@ -112,11 +123,81 @@ const FetchCategoryData = () => {
         AppendGifToPage();
     });
 
-    let CycleGif = setInterval(function() {
+    /*let CycleGif = setInterval(function() {
         index++;
         if(index > dataObjectNames.length) { index = 0; }
         AppendGifToPage();
-    }, 10000);
+    }, 10000);*/
+}
+
+const GenerateContentButtons = () => {
+    console.log("GENERATING USER INPUT HANDLERS");
+    var userInputDiv = document.createElement("div");
+    var buttonEl = document.createElement("button");
+    var labelEl = document.createElement("label");
+    var inputEl = document.createElement("input");
+
+    inputEl.setAttribute("id", "default-input");
+    inputEl.setAttribute("name", "default-input");
+    inputEl.setAttribute("type", "text");
+    inputEl.setAttribute("placeholder", "animals");
+
+    labelEl.setAttribute("for", "default-input");
+    labelEl.textContent = "Search: ";
+
+    buttonEl.textContent = "Search!";
+    buttonEl.addEventListener("click", function() {
+        if(inputEl.value == "") { 
+            console.log("No Search Param");
+            return; 
+        }
+        searchQuestion = inputEl.value;
+        HandleUserInput();
+    });
+    inputEl.addEventListener("submit", function(event) {
+        event.stopPropagation();
+        event.preventDefault();
+        searchQuestion = inputEl.value;
+        HandleUserInput();
+    });
+    
+    var selectLabelEl = document.createElement("label");
+    var selectEl = document.createElement("select");
+    selectLabelEl.textContent = "Choose Category: ";
+
+    selectEl.setAttribute("name", "category-selector");
+    selectEl.setAttribute("id", "category-selector");
+    selectLabelEl.setAttribute("for", "category-selector");
+
+    for(let i = 0; i < Object.keys(dataObjectNames).length; i++) {
+        var optionEl = document.createElement("option");
+        var toUpper = dataObjectNames[i];
+        toUpper = (toUpper.slice(0, 1)).toUpperCase() + toUpper.slice(1);
+        
+        optionEl.setAttribute("value", dataObjectNames[i]);
+        optionEl.textContent = toUpper;
+        selectEl.append(optionEl);
+    }
+
+    selectEl.addEventListener("change", function() {
+        var passVal = this.value;
+        console.log(passVal);
+
+        for(let i = 0; i < Object.keys(dataObjectNames).length; i++) {
+            if(passVal === dataObjectNames[i]) {
+                SetIndex(i);
+                HandleUserInput();
+            }
+        }
+    });
+    
+    userInputDiv.append(selectLabelEl);
+    userInputDiv.append(selectEl);
+
+    userInputDiv.append(labelEl);
+    userInputDiv.append(inputEl);
+    userInputDiv.append(buttonEl);
+    gifHolderEl.prepend(userInputDiv);
 }
 
 const AppendGifToPage = () => {
@@ -146,4 +227,9 @@ const AppendGifToPageAlt = () => {
     gifHolderEl.append(gifParent);
 }
 
-FetchSearchData();
+const HandleUserInput = () => {
+    FetchQuotes();
+    FetchSearchData();
+}
+
+GenerateContentButtons();
